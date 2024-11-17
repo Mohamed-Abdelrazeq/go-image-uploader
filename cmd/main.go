f53c618e220ff22115358feece7a6c155499d6b8
Mohamed-Abdelrazek-Mohamed <mabdelrazek@hindam.sa>
2024-11-17 09:26:27 +0200

    [Feature] upload image

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,41 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "OK",
-		})
-	})
+	r.GET("/health", health)
 
-	r.Run(":8080") // Listen and serve on 0.0.0.0:8080
+	r.POST("/upload", upload)
+
+	r.Run(":8080")
+}
+
+func upload(c *gin.Context) {
+	// parse image
+	file, err := c.FormFile("image")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to upload image",
+		})
+		return
+	}
+
+	// save image
+	err = c.SaveUploadedFile(file, "./images/"+file.Filename)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to save image",
+		})
+		return
+	}
+
+	// return response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Image uploaded successfully",
+		"url":     "http://localhost:8080/images/" + file.Filename,
+	})
+}
+
+func health(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
 }
