a934bec623b5561843f5985747c3489d6fb5d7a8
Mohamed-Abdelrazek-Mohamed <mabdelrazek@hindam.sa>
2024-11-21 12:08:34 +0200

    connect to s3

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+var (
+	s3Session *s3.S3
+	bucket    string
+)
+
+func initAWS() {
+	sess := session.Must(
+		session.NewSession(
+			&aws.Config{
+				Region: aws.String(os.Getenv("AWS_REGION")),
+			},
+		),
+	)
+	s3Session = s3.New(sess)
+	bucket = os.Getenv("AWS_BUCKET_NAME")
+}
 
 func main() {
 	r := gin.Default()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	initAWS()
 
 	r.GET("/health", health)
 
@@ -16,9 +44,7 @@ func main() {
 
 	r.Run(":8080")
 }
-
 func uploadImage(c *gin.Context) {
-	// parse image
 	file, err := c.FormFile("image")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -27,25 +53,38 @@ func uploadImage(c *gin.Context) {
 		return
 	}
 
-	// save image
-	err = c.SaveUploadedFile(file, "./images/"+file.Filename)
+	src, err := file.Open()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to save image",
+			"error": "Failed to open image",
+		})
+		return
+	}
+	defer src.Close()
+
+	_, err = s3Session.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(file.Filename),
+		Body:   src,
+		ACL:    aws.String("public-read"),
+	})
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to upload image to S3",
 		})
 		return
 	}
 
-	// return response
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Image uploaded successfully",
-		"url":     "http://localhost:8080/images/" + file.Filename,
+		"url":     "https://" + bucket + ".s3.amazonaws.com/" + file.Filename,
 	})
 }
 
 func showImage(c *gin.Context) {
 	filename := c.Param("filename")
-	c.File("./images/" + filename)
+	c.Redirect(http.StatusMovedPermanently, "https://"+bucket+".s3.amazonaws.com/"+filename)
 }
 
 func health(c *gin.Context) {
